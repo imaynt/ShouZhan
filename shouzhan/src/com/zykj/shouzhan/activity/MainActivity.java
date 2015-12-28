7429ffa468d0131122409d2812b27b606ca466b7
wyx <Administrator@WIN-LTHGLJKNC9R.lan>
2015-12-28 10:55:34 +0800

    wyx

---
+++ shouzhan/src/com/zykj/shouzhan/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.zykj.shouzhan.activity;
+
+import android.os.Bundle;
+
+import com.zykj.shouzhan.R;
+
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
